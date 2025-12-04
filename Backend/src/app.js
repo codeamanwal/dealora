44826f441650531c10a45c166c8f53078b94158c
kuldeepraj <kr9667102@gmail.com>
2025-12-04 08:02:01 +0530

    Added the urlencoder and also added the json limit for middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Backend is running!");
