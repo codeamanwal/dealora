f37dc58ce2e157202ef0f62b473f89470569a02a
kuldeepraj <kr9667102@gmail.com>
2025-12-04 08:03:55 +0530

    added cookie-parser middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Backend is running!");
