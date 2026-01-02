5f975caaf9e75afacb7bda26e0a5a771969b77e7
kuldeepraj <kr9667102@gmail.com>
2026-01-02 23:04:48 +0530

    installed the node cron so that it will scrap the coupon by own. no need to run the manual command.

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = require('./src/app');
 const { connectDB } = require('./src/config/database');
 const { validateEnv } = require('./src/config/env');
 const logger = require('./src/utils/logger');
+const { initCronJobs } = require('./src/cron/jobs');
 
 validateEnv();
 
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
     try {
         await connectDB();
+        initCronJobs();
 
         const server = app.listen(PORT, () => {
             logger.info(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
@@ -38,7 +40,7 @@ const startServer = async () => {
         });
 
         process.on('SIGTERM', () => {
-            logger.info('SIGTERM RECEIVED. Shutting down gracefully...');
+            logger.info('SIGTER M RECEIVED. Shutting down gracefully...');
             server.close(() => {
                 logger.info('Process terminated');
                 process.exit(0);
